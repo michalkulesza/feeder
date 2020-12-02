470478a82f3bd09f84c85b1b7f6d13f7d1814789
Michal <kulesza.michal@gmail.com>
2020-12-02 19:57:16 +0100

    refactor: function to const

--- src/components/PostsList/PostsList.js
+++ src/components/PostsList/PostsList.js
@@ -1,29 +1,27 @@
 import React from "react";
 import Post from "./Post";
 
-function PostsList({ posts, uid, handleDelete, username }) {
-  if (posts) {
-    return (
-      posts.length > 0 &&
-      posts.map(post => (
-        <Post
-          content={post.body}
-          author={post.author}
-          authorsUid={post.uid}
-          postId={post.id}
-          likes={post.likes}
-          likesUid={post.likesUid}
-          likesUsers={post.likesUsers}
-          currentUid={uid}
-          createdAt={post.createdAt}
-          key={post.id}
-          postKey={post.id}
-          handleDelete={handleDelete}
-          username={username}
-        />
-      ))
-    );
-  }
-}
+const PostsList = ({ posts, uid, handleDelete, username }) => {
+	return posts
+		? posts.length > 0 &&
+				posts.map(post => (
+					<Post
+						content={post.body}
+						author={post.author}
+						authorsUid={post.uid}
+						postId={post.id}
+						likes={post.likes}
+						likesUid={post.likesUid}
+						likesUsers={post.likesUsers}
+						currentUid={uid}
+						createdAt={post.createdAt}
+						key={post.id}
+						postKey={post.id}
+						handleDelete={handleDelete}
+						username={username}
+					/>
+				))
+		: null;
+};
 
 export default PostsList;
