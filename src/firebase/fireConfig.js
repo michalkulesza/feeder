e1e25a50503f7b4ac28731143a0fe46bac314a55
Michal <kulesza.michal@gmail.com>
2020-12-02 21:36:21 +0100

    misc

--- src/firebase/fireConfig.js
+++ src/firebase/fireConfig.js
@@ -2,7 +2,16 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/auth";
 import "firebase/storage";
-import { firebaseConfig } from "./config";
+
+const firebaseConfig = {
+	apiKey: "AIzaSyBbST9tnQvphoVEiD7oMn6s8zIOuarwJts",
+	authDomain: "feeder-f6976.firebaseapp.com",
+	databaseURL: "https://feeder-f6976.firebaseio.com",
+	projectId: "feeder-f6976",
+	storageBucket: "feeder-f6976.appspot.com",
+	messagingSenderId: "1019278937810",
+	appId: "1:1019278937810:web:23aa325af820884c84a70b",
+};
 
 firebase.initializeApp(firebaseConfig);
 
