aa1a88d3c5d1cc9fe0983f3586876547dd8fed9f
Michal <kulesza.michal@gmail.com>
2020-12-04 21:36:31 +0100

    test: added for PostMisc

--- src/components/PostMisc/PostMisc.js
+++ src/components/PostMisc/PostMisc.js
@@ -3,7 +3,7 @@ import "./PostMisc.scss";
 
 const PostMisc = ({ active, handleNewPost }) => {
 	return (
-		<div className={active ? "post-misc active" : "post-misc"}>
+		<div className={active === true ? "post-misc active" : "post-misc"}>
 			<span></span>
 			<div className="right">
 				<div className="button green" onClick={handleNewPost}>
